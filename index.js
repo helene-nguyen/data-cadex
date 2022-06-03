8c59ed17c04448f65e31e547f92c9480b0142e20
Yumicode <75518128+helene-nguyen@users.noreply.github.com>
2022-06-03 14:19:11 +0200

    [feat] BACK create little API + FRONT fetch data render random sentences

--- index.js
+++ index.js
@@ -8,17 +8,33 @@ const app = express();
 // source : https://stackoverflow.com/questions/70106880/err-import-assertion-type-missing-for-import-of-json-file
 import parts from './data/parts.json' assert {type: "json"};
 app.locals.parts = parts;
+//~Protect our API
+import helmet from 'helmet';
+app.use(helmet());
+
+//~url encoded
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+//~Allow CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE");
+    next();
+}); 
 
 //~Import router
 import { router } from './app/routes.js'
 import { _404 } from './app/controllers/errorController.js';
 
-
 //~Route
 app.use(router);
 
 //~Error
-// app.use(_404);
+app.use(_404);
 
 //~Launch the app
 const PORT = process.env.PORT ?? 3000;
--- public/js/script.js
+++ public/js/script.js
@@ -5,9 +5,10 @@ const app = {
 
   //^INITIALIZATION
   async init() {
-    console.log("TEST",location.origin);
+    console.log('TEST', location.origin);
     await app.fetchCadex();
     await app.tryPost();
+    await app.fetchAllCadex();
     document.querySelector('#again').addEventListener('click', app.fetchCadex);
     document.querySelector('.formOpen').addEventListener('click', app.showForm);
     document.querySelector('.formClose').addEventListener('click', app.hideForm);
@@ -93,11 +94,46 @@ const app = {
 
   //récupération d'un cadex aléatoire ou configuré via la queryString
   //cette string est stockée dans la variable location.search, on la retransmet telle quelle à la route
-  async fetchCadex(){
+  async fetchCadex() {
     try {
       const response = await fetch(`${app.baseUrl}/cadex${location.search}`);
       const phrase = await response.json();
       app.displayPhrase(phrase);
+    } catch (error) {
+      console.error(error);
+    }
+    },
+  
+//^Testing json file
+  async fetchAllCadex() {
+    try {
+      const response = await fetch('http://localhost:4100/v1');
+
+      if (response.ok) {
+        const tableBodyElement = document.querySelector('tbody');
+        const parts = await response.json();
+  
+        let completeSentence;
+
+        for (const name of parts.names) {
+          const template = document.querySelector('#template-sentence');
+          const clone = document.importNode(template.content, true);
+          const rowElement = clone.querySelector('tr');
+
+          //~get random element from array
+          //source https://stackoverflow.com/questions/4550505/getting-a-random-value-from-a-javascript-array
+          const randomName = parts.names[Math.floor(Math.random() * parts.names.length)];
+          const randomAdjectives = parts.adjectives[Math.floor(Math.random() * parts.adjectives.length)];
+          const randomVerbs = parts.verbs[Math.floor(Math.random() * parts.verbs.length)];
+          const randomComplements = parts.complements[Math.floor(Math.random() * parts.complements.length)];
+
+          //~write the sentence
+          completeSentence = rowElement.querySelector(
+            '.sentence'
+          ).textContent = `${randomName} ${randomAdjectives} ${randomVerbs} ${randomComplements}`;
+          tableBodyElement.appendChild(rowElement);
+        }
+      }
     } catch (error) {
       console.error(error);
     }

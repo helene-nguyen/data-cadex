6909e10f07e7712ec30d831f7711e726561969a5
Yumicode <75518128+helene-nguyen@users.noreply.github.com>
2022-06-14 07:48:16 +0200

    [feat] for deploy

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,7 +1,7 @@
 //~import modules
 import { _500 } from './errorController.js';
 import { Cadex } from '../models/cadex.js';
-import { createData } from '../datamapper/cadex.js';
+// import { createData } from '../datamapper/cadex.js';
 
 //~controller
 async function fetchAllCadex(req, res, next) {
@@ -22,7 +22,7 @@ async function doRandomCadex(req, res) {
     //https://stackoverflow.com/questions/53215853/how-to-add-a-query-string-to-req-url-in-express
     let cadex = [];
 
-    await createData(req.body);
+    // await createData(req.body);
 
     for (const [key, value] of Object.entries(random)) {
       let inputFilled = req.body[key];
